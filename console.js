ba0c2028cd1c18ed92da10c909ff2d310a8004af
motinha122 <timota132@gmail.com>
2022-08-20 14:11:58 -0300

    process node

--- console.js
+++ console.js
@@ -1,3 +1,24 @@
-const getFlagValue = require('./require');
+const questions = [
+    "Qual a cor da sua calcinha?",
+    "Qual o tamanho que você usa?",
+    "Prefere renda, box ou fio dental?",
+    "Bordada ou sem estampa?",
+];
 
-console.log(`Hello ${getFlagValue('--name')}, ${getFlagValue('--greeting')}`)
+const ask = (index = 0) => {
+    process.stdout.write(questions[index] + '\n');
+};
+
+ask();
+
+const answers = [];
+
+process.stdin.on("data", data => {
+    answers.push(data.toString().trim())
+    if(answers.length < questions.length){
+        ask(answers.length)
+    } else {
+        console.log(answers)
+        process.exit();
+    }
+})
--- require.js
+++
@@ -1,6 +0,0 @@
-function getFlagValue(flag) {
-    const index = process.argv.indexOf(flag);
-    return process.argv[index + 1]
-}
-
-module.exports = getFlagValue;
