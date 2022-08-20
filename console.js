ce1ebe28239ba9bc9a67809be947deb6d78fc6f2
motinha122 <timota132@gmail.com>
2022-08-20 15:31:09 -0300

    timeout

--- console.js
+++ console.js
@@ -1,28 +1,8 @@
-const questions = [
-    "Qual a cor da sua calcinha?",
-    "Qual o tamanho que você usa?",
-    "Prefere renda, box ou fio dental?",
-    "Bordada ou sem estampa?",
-]
+const print = (...p) => console.log(...p); 
 
-const ask = (index = 0) => {
-    process.stdout.write(questions[index] + '\n');
-}
+const timeout = 3000;
+const finished = () => console.log('done');
 
-ask();
+setTimeout(finished,timeout);
 
-process.on('exit',()=>{
-    console.log("Maneiro cara, você tem um bom gosto :)");
-})
-
-const answers = [];
-
-process.stdin.on("data", data => {
-    answers.push(data.toString().trim())
-    if(answers.length < questions.length){
-        ask(answers.length);
-    } else {
-        console.log(answers);
-        process.exit();
-    }
-})
+print('Xampson','Batata');
