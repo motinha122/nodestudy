d944b74401dc50edae9b887bb853974ec0ea1971
motinha122 <timota132@gmail.com>
2022-08-25 23:49:56 -0300

    object index

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const app = express();
 app.set("view engine","ejs");
 
 app.get("/", function(req, res){
-    res.render("index");
+    const items = [
+        {
+            title: "D",
+            message: "Developar aplications especializadas em crashar"
+        },
+        {
+            title: "E",
+            message: "Eu uso EJS e Javascript pra quebrar HTML renderizado"
+        },
+        {
+            title: "M",
+            message: "Muitos códigos quebrados"
+        },
+    ];
+    res.render("pages/index",{
+        qualitys: items,
+    });
 })
 
 app.get("/sobre", function(req, res){
-    res.render("about");
+    res.render("pages/about");
 })
 
 app.listen(666);
