1e2e2f1eb2cc2df779e5f4069342f896e0243b7e
motinha122 <timota132@gmail.com>
2022-08-29 11:48:59 -0300

    estrutura de dados

---
+++ Estrutura de dados/console.js
@@ -0,0 +1,13 @@
+const pilotos = ['Senna','Prost','Schumacher','Hamilton'];
+
+pilotos.push('Alonso');
+pilotos.pop();
+
+
+const senna = pilotos.find(piloto => piloto === 'Senna');
+
+console.log(senna);
+
+pilotos.splice(1,1); //remove 1 item na posição 1
+
+console.log(pilotos);
---
+++ Estrutura de dados/console3.js
@@ -0,0 +1,26 @@
+// Queue FIFO
+
+const { fileLoader } = require("ejs");
+
+class Queue{
+    constructor(){
+        this.data = [];
+    }
+    enqueue(item){
+        this.data.push(item);
+        console.log(`${item} entrou na fila`);
+    }
+    dequeue(){
+        const item = this.data.shift();
+        console.log(`${item} saiu da fila`)
+    }
+}
+
+const fila = new Queue();
+
+fila.enqueue('batata');
+fila.enqueue('cenoura');
+fila.enqueue('carne');
+fila.dequeue();
+fila.dequeue();
+fila.dequeue();
