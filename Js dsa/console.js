9e75fc2a31e4cdaf669c0cb07ed0df2ebdd8bbfd
motinha122 <timota132@gmail.com>
2022-09-04 16:48:33 -0300

    fixes

--- Js dsa/console.js
+++ Js dsa/console.js
@@ -10,3 +10,16 @@ function cubic(n) {
     return counter;
 }
 console.log(cubic(4));
+
+function quadratic(n) {
+    let counter = 0;
+    for (let i = 0 ; i < n ; i++){
+        for (let j = 0 ; j < n ;j++){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+console.log(quadratic(4));
+
