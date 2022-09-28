e00a257fec1cb1f381e75bdc1e8424ed7bd7c7e0
motinha122 <timota132@gmail.com>
2022-09-28 15:51:20 -0300

    bubblesort

---
+++ Js dsa/bubbleSort.js
@@ -0,0 +1,26 @@
+function BubbleSort(lista){
+    let aux = 0;
+    let iterations = 0;
+
+    for(i = lista.length; i > 0; i--){
+        for(j = 0; j < (lista.length-1); j++){
+            if(lista[j] > lista[j+1]){
+                aux = lista[j+1];
+                lista[j+1] = lista[j];
+                lista[j] = aux;
+            }
+            iterations++;
+        }
+    }
+    console.log(iterations);
+    return lista;
+}
+
+
+let lista = [7,5,1,8,3];
+let lista1 = [117,90,69,69,8,6,5];
+let lista2 = [7,7,7,7,1,1,1,9,9,9,0,4,5,7,1];
+
+console.log(BubbleSort(lista));
+console.log(BubbleSort(lista1));
+console.log(BubbleSort(lista2));
--- Js dsa/selectionsort.js
+++ Js dsa/selectionsort.js
@@ -1,9 +1,11 @@
 function SelectionSort(lista){
+    let iterations = 0;
     for(let j = 0; j < (lista.length-1); j++){
         let minIndex = j;
         for(let i = j; i < lista.length; i++){
             if(lista[i] < lista[minIndex]){
                 minIndex = i;
+                iterations++;
             }
         }
         if(lista[j] > lista[minIndex]){
@@ -11,7 +13,9 @@ function SelectionSort(lista){
             lista[j] = lista[minIndex];
             lista[minIndex] = aux;
         }
+        iterations++;
     }
+    console.log(iterations);
     return lista;
 }
 
@@ -19,9 +23,6 @@ let lista = [7,5,1,8,3];
 let lista1 = [117,90,69,69,8,6,5];
 let lista2 = [7,7,7,7,1,1,1,9,9,9,0,4,5,7,1];
 
-let listinha = SelectionSort(lista);
-
 console.log(SelectionSort(lista));
 console.log(SelectionSort(lista1));
 console.log(SelectionSort(lista2));
-console.log(listinha);
